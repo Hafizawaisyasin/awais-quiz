27a230dc65b338e2aa9764365456aad233a1b6df
Hafizawaisyasin <68225696+Hafizawaisyasin@users.noreply.github.com>
2020-11-04 02:23:16 +0500

    a

--- app.js
+++ app.js
@@ -4,7 +4,18 @@ const result = document.querySelector(".result");
 form.addEventListener("submit", (e) => {
 	e.preventDefault();
 	let score = 0;
-	const userAns = [form.q1.value, form.q2.value, form.q3.value, form.q4.value];
+	const userAns = [
+		form.q1.value,
+		form.q2.value,
+		form.q3.value,
+		form.q4.value,
+		form.q5.value,
+		form.q6.value,
+		form.q7.value,
+		form.q8.value,
+		form.q9.value,
+		form.q10.value,
+	];
 
 	userAns.forEach((ans, index) => {
 		if (ans === correctAnswer[index]) {
